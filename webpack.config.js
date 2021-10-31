dfa9230640980fd9b1ceb660b1ee3fd8179022e3
Gustavo Canedo <gustavo.canedo@betrybe.com>
2021-10-31 15:35:45 -0300

    dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
   },
 };
